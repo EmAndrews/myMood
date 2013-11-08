6562e144cda745d7bcdb7db4a6aea92d4667f812
kat <kataleksandrova@gmail.com>
2013-11-08 14:03:54 -0800

    fixing precompiled assets thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,9 +15,7 @@ Mm::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  config.assets.precompile += %w( bootstrap.min.css bootstrap-responsive.min.css index.css jquery-ui.css application.js bootstrap.min.js d3.v3.min.js index.js jquery-1.10.2.min.js jquery-ui.js )
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
