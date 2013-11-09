0f4d3cdbec5baf53ce751b1f9beefc3d7dfb0006
Andre Crabb <crabb.andre@gmail.com>
2013-11-09 13:02:50 -0800

    Typed functions for scheduling messages

---
+++ app/models/schedule_preference.rb
@@ -0,0 +1,16 @@
+# Some schedule stuff
+#
+# belongs_to user
+
+
+module Weekday
+  SUNDAY    = 1 << 0
+  MONDAY    = 1 << 1
+  TUESDAY   = 1 << 2
+  WEDNESDAY = 1 << 3
+  THURSDAY  = 1 << 4
+  FRIDAY    = 1 << 5
+  SATURDAY  = 1 << 6
+end
+
+# 0 | MONDDAY | TUESDAY
