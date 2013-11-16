ca858ab0a67ec7ef24c60c8d859539aa3b4ea305
Andre Crabb <crabb.andre@gmail.com>
2013-11-16 15:59:27 -0800

    utils and things

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,11 +1,18 @@
 require 'clockwork'
+require_relative '../app/Util.rb'
+#require_relative '../app/controllers/sms_controller.rb'
 include Clockwork
+include Twilio
+
 
 handler do |job|
   puts "Running #{job}"
 end
 
-every(10.seconds, 'frequent.job')
+every(1.minute, 'frequent.job') do 
+  Util.send_message "+18586921233", "HI ME"
+end
+
 #every(3.minutes, 'less.frequent.job')
 #every(1.hour, 'hourly.job')
 
--- lib/util.rb
+++ lib/util.rb
@@ -1,5 +1,9 @@
-class Util
+module Util
 
+  def self.test
+    puts 'UTIL TESTING'
+  end
+  
 	#pass in "2223334444" or "222-333-4444" or "(222) 333-4444" and get back "+12223334444"
 	def self.convert_phone (number)
 		phone_regex = /^\(*(?<start>\d{3})\)*-*\s*(?<middle>\d{3})-*\s*(?<end>\d{4})/
@@ -12,6 +16,7 @@ class Util
 
 
 	def self.send_message(to, message)
+    puts ">>Util>> Sending message #{message} to #{to}"
 	  sid = 'ACb85e0121426b1e833e86822cc2800cb6'
 	  token =  'dc7e939dfe23d90dc37644173b7e7415'
 		client = Twilio::REST::Client.new sid, token
