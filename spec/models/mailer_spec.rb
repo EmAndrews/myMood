e573426dca53d75569a9e6589c9ff8a8bac945cc
Kim <saasbook@saasbook.(none)>
2013-11-17 15:02:12 -0800

    rspec syntax errors

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -21,22 +21,23 @@ describe Mailer do
 
   describe 'daily_cron' do
     it 'should iterate over every user' do
-      User.should_recieve(:where).and_return([@fake_user1, @fake_user2])
+      User.should_receive(:where).and_return([@fake_user1, @fake_user2])
       @fake.should_receive(:prep_messages).with(@fake_user1)
       @fake.should_receive(:prep_messages).with(@fake_user2)
       
     end
     it 'should error on nil user' do
-      User.should_recieve(:where).and_return([@nil_user])
-      @fake should_recieve(:prep_messages).with(@nil_user)
-      logger.should_recieve(:debug)
+      User.should_receive(:where).and_return([@nil_user])
+      @fake.should_receive(:prep_messages).with(@nil_user)
+      logger.should_receive(:debug)
       prep_messages(@nil_user)
     end
+  end
 
   describe 'prep_messages' do
     it 'should process the messages queue' do
-      @fake_user1.should_receieve(:message_queue).and_return([@message1, @message2])
-      @fake_user1.message_queue.should_recieve(:each)
+      @fake_user1.should_receive(:message_queue).and_return([@message1, @message2])
+      @fake_user1.message_queue.should_receive(:each)
       @fake.should_receive(:enqueue_next_message).with(@message1, @fake_user1)
       @fake.should_receive(:enqueue_next_message).with(@message2, @fake_user1)
       @fake.should_receive(:send_message_to_user).with(@message1, @fake_user1)
@@ -47,7 +48,7 @@ describe Mailer do
 
   describe 'enqueue_next_message' do
     it 'should get the next sequence number' do
-      @fake_user1.should_recieve(:user_states).and_return({:seq_num => 3})
+      @fake_user1.should_receive(:user_states).and_return({:seq_num => 3})
       @message1.should_receive(:category).and_return(@category1)
       @category1.should_receive(:get_next_message_and_num).with(3).and_return(@next_message, 4)
       @fake_user1.message_queue.should_receive(:add)
