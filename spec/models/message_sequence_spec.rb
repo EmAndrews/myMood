36a7fb715aa982a6d570af34f66d23d7169ca7a1
Sargun Kaur <sargun.kaur@berkeley.edu>
2013-11-20 23:38:14 -0800

    model pending specs

---
+++ spec/models/message_sequence_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Message Sequence do
+	it "should have a message sent from the admin with the boolean true"
+	it "should have the user replied message content with the boolean set to false"
+	it "the message should have a category id"
+end 
+
---
+++ spec/models/processed_messages_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Processed Messages do 
+	it "has a user_id"
+	it "has a string text"
+	it "belongs to a category"
+end
+
