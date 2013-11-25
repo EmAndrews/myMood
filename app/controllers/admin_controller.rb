50d0f99ec3c30dd02a4ad8923cf29ab5953ce8f2
kat <kataleksandrova@gmail.com>
2013-11-24 17:09:46 -0800

    trying to make RegistrationsController less complex

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,8 +3,6 @@ include Util
 
 class AdminController < ApplicationController
 
-#logger = Logger.new('log/andre.txt')
-
   def index
     Rails.logger.debug('Hi Admin')
     @messages = MessageTemplate.all
@@ -53,21 +51,6 @@ class AdminController < ApplicationController
     flash[:notice] = "Message '#{message_text}' added in category '#{message[:category]}'"
     redirect_to admin_path
   end
-
-#begin
-#  def new_category category
-#    cat_name = category[:name]
-#    Category.create!(:name => cat_name)
-#    flash[:notice] = "Category '#{cat_name}' created!"
-#    redirect_to admin_path
-#  end
-#
-#  def new_message message
-#    message_text = message[:text]
-#    MessageTemplate.create!(:text => message[:text], :category => Category.find_by_name(message[:category]))
-#    flash[:notice] = "Message '#{message_text}' added with category '#{message[:category]}'!"
-#    redirect_to admin_path
-#  end
 
   def delete_something
     if params[:message_id]
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,42 +1,26 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  #protected
-
   def update
-
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
     if update_resource(resource, account_update_params)
-      yield resource if block_given?
-      if is_flashing_format?
-        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-            :update_needs_confirmation : :updated
-        set_flash_message :notice, flash_key
-      end
-      sign_in resource_name, resource, :bypass => true
-      respond_with resource, :location => profile_url(:phone_number => resource.phone_number)
+      change_password(prev_unconfirmed_email)
     else
       clean_up_passwords resource
       flash[:alert] = 'Failed to update settings'
-      #set_flash_message :notice,
       redirect_to profile_url(:phone_number => resource.phone_number)
-      #respond_with resource, :location => '/'
-      #respond_with resource, :location => profile_url(:phone_number => resource.phone_number)
     end
-    #
-    # Devise use update_with_password instead of update_attributes.
-    # This is the only change we make.
+  end
 
-    #if resource.update_attributes(params[resource_name])
-    #  set_flash_message :notice, :updated
-    #  # Line below required if using Devise >= 1.2.0
-    #  sign_in resource_name, resource, :bypass => true
-    #  redirect_to after_update_path_for(resource)
-    #else
-    #  clean_up_passwords(resource)
-    #  respond_with resource, :location => after_update_path_for(resource)
-    #  #profile_url(:phone_number => resource.phone_number)
-    #end
+  def change_password(prev_unconfirmed_email)
+    yield resource if block_given?
+    if is_flashing_format?
+      flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+      set_flash_message :notice, flash_key
+    end
+    sign_in resource_name, resource, :bypass => true
+    respond_with resource, :location => profile_url(:phone_number => resource.phone_number)
   end
 end
