117e84fa55be08ac219c214a8621fb9f524300ed
Emily Andrews <emily@tesba.net>
2013-12-01 18:15:04 -0800

    time shit

--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -7,11 +7,19 @@ describe MessageSendingJob do
     mood_cat.save!
     @user1 = User.create!(:name => "User1", :subscription => {mood_cat.id => {}}, :phone_number => '111-111-1111', :password => "user1_pass", :email => 'user1@email.com', :availability => {'M' => 1, 'Tu' => 1, 'W' => 1, 'F' => 1, 'Sa' => 0, 'Su' => 1})
     @user2 = User.create!(:name => "User2", :subscription => {mood_cat.id => {}}, :phone_number => '222-222-2222', :password => "user2_pass", :email => 'user2@email.com', :availability => {'M' => 1, 'Tu' => 1, 'W' => 1, 'F' => 1, 'Sa' => 1, 'Su' => 0})
+    Time.stub!(:now).and_return(Time.mktime(2013, 11, 30))
   end
-
+  
+  # @Andre/Alex, I figured out how to set the time to 
+  # the day we want:
+  # Time.stub!(:now).and_return(Time.mktime(year, month, day))
+  # Now when we call Time.now, the time we created will be returned.
+  # Bam.
+  
   describe 'perform' do
     it 'should iterate through all users' do
         User.should_receive(:where).and_return([@user1, @user2])
+        p Time.now
     end
     
     it 'should enque a message for user1 if today is not saturday' do
