318d20cdc0e68077cff12815267d2f67ceeec9ec
Kim <saasbook@saasbook.(none)>
2013-12-06 21:57:13 -0800

    csv working, see /admin/download

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -5,26 +5,8 @@ class DownloadController < ApplicationController
 	# http://railscasts.com/episodes/362-exporting-csv-and-excel?view=asciicast
 
 	def download
-		print "download called\n"
-    @messages = ProcessedMessages.all #ProcessedMessages.all  #Products.order(:name) ?
-    respond_to do |format|
-    	print "entered block\n"
-      format.html
-      print "format.html\n"
-      mess = @messages.to_csv
-      print "mess: ", mess, "\n"
-      format.csv { send_data mess }
-      print "sent\n"
-    end
-  end
-  
-  def my_csv (all_stuff)
-  	print "my_csv"
-  	CSV.generate do |csv|
-		  csv << column_names
-		  all_stuff.each do |product|
-		    csv << user.attributes.values_at(*column_names)
-		  end
-		end
+		csv_file = ProcessedMessages.gen_csv()
+		send_data csv_file, :type => 'text/csv; charset=iso-8859-1; header=present', :disposition => "attachment; filename=test.csv"
+
 	end
 end
--- app/models/processed_messages.rb
+++ app/models/processed_messages.rb
@@ -1,17 +1,16 @@
 class ProcessedMessages < ActiveRecord::Base
 	attr_accessible :text, :data, :from_my_mood, :date_processed
 	belongs_to :user 
-	has_one :category 
+	has_one :category
 	
-	def self.to_csv(options = {})
-		print "testing"
+	def self.gen_csv(options = {})
+		print "to_csv called"
 		CSV.generate(options) do |csv|
 		  csv << column_names
-		  all.each do |product|
-		    csv << product.attributes.values_at(*column_names)
+		  all.each do |message|
+		    csv << message.attributes.values_at(*column_names)
 		  end
 		end
-	end
-  
+  end
 end
 
