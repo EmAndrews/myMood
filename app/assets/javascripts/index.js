6df9636165a7108c34026896719bcbd986d70c6b
kat <kataleksandrova@gmail.com>
2013-12-08 13:53:07 -0800

    added id's to delete btns

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,47 +1,6 @@
 $(function () {
     var h = new Highcharts.Chart({
         chart: {
-            renderTo: 'line-graph',
-            type: 'line'
-        },
-        title: {
-            text: 'Week of Ratings',
-            x: -20 //center
-        },
-        xAxis: {
-            categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-        },
-        yAxis: {
-            title: {
-                text: 'Ratings'
-            },
-            max: 10,
-            tickInterval: 1,
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Mood',
-            data: [7, 6, 9, 10, 8, 2, 5]
-        }, {
-            name: 'Sleep',
-            data: [2, 8, 5, 10, 7, 2, 4]
-        }]
-    });
-});
-
-$(function () {
-    var h = new Highcharts.Chart({
-        chart: {
             renderTo: 'pie-chart',
             type: 'pie',
             plotBackgroundColor: null,
@@ -171,12 +130,94 @@ function drawAdminPieChartIfAdmin() {
 }
 $(document).ready(function () {
 
-    var user_msgs;
+    var user_msgs = null;
     $.get('/user_messages',function(data) {
         user_msgs = data;
+        console.log("data was successfully received from the ajax request");
     }, 'json');
 
-    console.log(user_msgs);
+    if (user_msgs != null) {
+        $(function () {
+            var h = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'line-graph',
+                    type: 'line'
+                },
+                title: {
+                    text: 'Week of Ratings',
+                    x: -20 //center
+                },
+                xAxis: {
+                    categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Ratings'
+                    },
+                    max: 10,
+                    min: 0,
+                    tickInterval: 1,
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                },
+                series: [{
+                    name: 'Mood',
+                    data: [7, 6, 9, 10, 8, 2, 5]
+                }, {
+                    name: 'Sleep',
+                    data: [2, 8, 5, 10, 7, 2, 4]
+                }]
+            });
+        });
+    } else {
+        $(function () {
+            var h = new Highcharts.Chart({
+                chart: {
+                    renderTo: 'line-graph',
+                    type: 'line'
+                },
+                title: {
+                    text: 'Week of Ratings',
+                    x: -20 //center
+                },
+                xAxis: {
+                    categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Ratings'
+                    },
+                    max: 10,
+                    min: 0,
+                    tickInterval: 1,
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                },
+                series: [{
+                    name: 'Mood',
+                    data: [0, 0, 0, 0, 0, 0, 0]
+                }]
+            });
+        });
+    }
 
     drawAdminPieChartIfAdmin();
 
